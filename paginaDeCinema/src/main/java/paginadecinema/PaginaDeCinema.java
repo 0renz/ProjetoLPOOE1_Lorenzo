ca324a13f5628435fac99d4cdc5e574fdb6d42df
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2024-07-12 14:47:39 -0300

    Configura arquivo de persistência + renomeia main

--- paginaDeCinema/src/main/java/paginadecinema/PaginaDeCinema.java
+++ paginaDeCinema/src/main/java/paginadecinema/PaginaDeCinema.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package projetolpooe1_lorenzo.paginadecinema;
+package paginadecinema;
 
 /**
  *
