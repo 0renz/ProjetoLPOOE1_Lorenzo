bebbe6f7513c06ed14fc8f6f90d2fa4e85d15336
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2024-07-12 14:30:13 -0300

    Primeiro commit

---
+++ paginaDeCinema/src/main/java/projetolpooe1_lorenzo/paginadecinema/PaginaDeCinema.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package projetolpooe1_lorenzo.paginadecinema;
+
+/**
+ *
+ * @author loren
+ */
+public class PaginaDeCinema {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
