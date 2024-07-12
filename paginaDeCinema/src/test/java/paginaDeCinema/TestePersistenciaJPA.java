bbe67510783cdbf2a2ce370f79555e734454fc09
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2024-07-12 16:05:48 -0300

    Adiciona modelos

---
+++ paginaDeCinema/src/test/java/paginaDeCinema/TestePersistenciaJPA.java
@@ -0,0 +1,23 @@
+package paginaDeCinema;
+
+import java.util.Calendar;
+import java.util.Date;
+import org.junit.Test;
+import paginadecinema.modelo.dao.PersistenciaJPA;
+
+public class TestePersistenciaJPA {
+      @Test
+    public void testConexaoJPA() {
+        //criar um objeto do tipo PersistenciaJPA.
+        PersistenciaJPA jpa = new PersistenciaJPA();
+        if (jpa.conexaoAberta()) {
+            System.out.println("conectou no BD via jpa ...");
+            jpa.fecharConexao();
+        } else {
+            System.out.println("nao conectou no BD ...");
+
+        }
+    } 
+}    
+
+   
