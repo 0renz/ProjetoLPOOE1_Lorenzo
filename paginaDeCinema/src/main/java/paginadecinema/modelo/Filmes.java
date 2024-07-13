1a594be98dc62f3c2b78e70f2892084eb3ffb914
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2024-07-13 11:42:55 -0300

    Modela classes e suas respectivas representações em JPA

--- paginaDeCinema/src/main/java/paginadecinema/modelo/Filmes.java
+++ paginaDeCinema/src/main/java/paginadecinema/modelo/Filmes.java
@@ -1,13 +1,105 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package paginadecinema.modelo;
 
-/**
- *
- * @author loren
- */
-public class Filmes {
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Calendar;
+import java.util.List;
+
+@Entity
+@Table (name = "tb_filmes")
+public class Filmes implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idFilme;
+
+    @Column(nullable = false, length = 155)
+    private String nomeFilme;
     
+    @Column(nullable = false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Calendar dataEstreia;
+    
+    @Column(nullable = false)
+    private Double notaIMDb;
+
+    @ManyToMany
+    @JoinTable(
+        name = "Filme_Roteirista",
+        joinColumns = @JoinColumn(name = "idFilme"),
+        inverseJoinColumns = @JoinColumn(name = "idRoteirista")
+    )
+    private List<Roteiristas> roteiristas;
+
+    @ManyToMany
+    @JoinTable(
+        name = "Filme_Ator",
+        joinColumns = @JoinColumn(name = "idFilme"),
+        inverseJoinColumns = @JoinColumn(name = "idAtor")
+    )
+    private List<Atores> atores;
+
+    @ManyToMany
+    @JoinTable(
+        name = "Filme_Diretor",
+        joinColumns = @JoinColumn(name = "idFilme"),
+        inverseJoinColumns = @JoinColumn(name = "idDiretor")
+    )
+    private List<Diretores> diretores;
+
+
+    public Integer getIdFilme() {
+        return idFilme;
+    }
+
+    public void setIdFilme(Integer idFilme) {
+        this.idFilme = idFilme;
+    }
+
+    public String getNomeFilme() {
+        return nomeFilme;
+    }
+
+    public void setNomeFilme(String nomeFilme) {
+        this.nomeFilme = nomeFilme;
+    }
+
+    public Calendar getDataEstreia() {
+        return dataEstreia;
+    }
+
+    public void setDataEstreia(Calendar dataEstreia) {
+        this.dataEstreia = dataEstreia;
+    }
+
+    public Double getNotaIMDb() {
+        return notaIMDb;
+    }
+
+    public void setNotaIMDb(Double notaIMDb) {
+        this.notaIMDb = notaIMDb;
+    }
+
+    public List<Roteiristas> getRoteiristas() {
+        return roteiristas;
+    }
+
+    public void setRoteiristas(List<Roteiristas> roteiristas) {
+        this.roteiristas = roteiristas;
+    }
+
+    public List<Atores> getAtores() {
+        return atores;
+    }
+
+    public void setAtores(List<Atores> atores) {
+        this.atores = atores;
+    }
+
+    public List<Diretores> getDiretores() {
+        return diretores;
+    }
+
+    public void setDiretores(List<Diretores> diretores) {
+        this.diretores = diretores;
+    }
 }
